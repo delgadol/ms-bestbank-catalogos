5043f1dc8f697069d39fe62d0d1f56961181e194
Luis Delgado <37703993+delgadol@users.noreply.github.com>
2023-07-30 14:02:09 -0500

    Initial commit

---
+++ src/main/java/com/bestbank/catalogos/MsBestbankCatalogosApplication.java
@@ -0,0 +1,13 @@
+package com.bestbank.catalogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBestbankCatalogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBestbankCatalogosApplication.class, args);
+	}
+
+}
